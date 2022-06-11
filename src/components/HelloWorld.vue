b9aad27abebdeba88eb34ec1c386dc7fe88e06c8
dmitriev dmitrii <dmitriev.dmitrii@mail.com>
2022-06-11 22:29:11 +0300

    added-vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div class="input-wrap">
+    <h1>{{value}}</h1>
+    <input class="input" @input="jump" @change="jump" type="range" v-model="value" max="1000"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      value: 0,
+    };
+  },
+  methods: {
+    jump: function (event) {
+      console.log(event.type);
+      console.log(event.target.value);
+      this.value = event.target.value;
+    },
+  },
+};
+</script>
+
+<style >
+.input-wrap{
+  width: 500px;
+  margin: 0 auto;
+}
+.input{
+  width: 100%;
+  cursor: pointer;
+}
+</style>
